ad69a44419c80da007ae07e95aea449b20d446fd
Alan-Abdul-Kalaam-25 <alanabdulkalaam@gmail.com>
2024-09-15 16:33:09 +0530

    inital commit

---
+++ src/components/Webapp.jsx
@@ -0,0 +1,11 @@
+const Webapp = () => {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>Servies</li>
+      <li>About</li>
+    </ul>
+  );
+};
+
+export default Webapp;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Webapp from "./components/Webapp.jsx";
+
+function App() {
+  return (
+    <>
+      <Webapp />
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
